b9fb7f65eafad489b15d1c308cf5f7e93d0cf87d
Nguyen Ngoc Han <nguyenngochandeveloper@gmail.com>
2026-04-13 03:34:38 +0700

    feat: add session check and redirect logic to login routes for admin, candidate, and employer apps

--- apps/candidate/src/routes/login.tsx
+++ apps/candidate/src/routes/login.tsx
@@ -1,10 +1,21 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useState } from "react";
 
 import SignInForm from "@/components/sign-in-form";
 import SignUpForm from "@/components/sign-up-form";
+import { authClient } from "@/lib/auth-client";
+
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: async () => {
+    const session = await authClient.getSession();
+    if (session.data) {
+      redirect({
+        to: "/home",
+        throw: true,
+      });
+    }
+  },
   component: RouteComponent,
 });
 
--- apps/employer/src/routes/login.tsx
+++ apps/employer/src/routes/login.tsx
@@ -1,13 +1,23 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useState } from "react";
 
 import SignInForm from "@/components/sign-in-form";
 import SignUpForm from "@/components/sign-up-form";
+import { authClient } from "@/lib/auth-client";
+
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: async () => {
+    const session = await authClient.getSession();
+    if (session.data) {
+      redirect({
+        to: "/dashboard",
+        throw: true,
+      });
+    }
+  },
   component: RouteComponent,
 });
-
 function RouteComponent() {
   const [showSignIn, setShowSignIn] = useState(true);
 
